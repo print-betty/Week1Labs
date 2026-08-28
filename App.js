053ed44347523897a05d5abd01a8e17da3fd0576
print-betty <156491655+print-betty@users.noreply.github.com>
2026-08-28 23:33:57 +0800

    Lab 5: Navigation between Welcome and AddTask screens

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import WelcomeScreen from './screens/WelcomeScreen';
 import AddTaskScreen from './screens/AddTaskScreen';
-
+const Stack = createNativeStackNavigator();
 export default function App() {
-return <AddTaskScreen />;
 
+return (
+<NavigationContainer>
+
+<Stack.Navigator initialRouteName="Welcome">
+
+<Stack.Screen
+name="Welcome"
+component={WelcomeScreen}
+options={{ headerShown: false }}
+
+/>
+<Stack.Screen
+name="AddTask"
+component={AddTaskScreen}
+options={{ title: 'My Tasks' }}
+
+/>
+</Stack.Navigator>
+</NavigationContainer>
+
+);
 }
