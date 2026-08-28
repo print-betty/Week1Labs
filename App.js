e93e3939f7ead55a3d2c587525ab9f3fa23416d9
print-betty <156491655+print-betty@users.noreply.github.com>
2026-08-28 23:28:13 +0800

    Lab 4: State, hooks, and user input with AddTaskScreen

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import WelcomeScreen from './screens/WelcomeScreen';
+import AddTaskScreen from './screens/AddTaskScreen';
 
 export default function App() {
-return <WelcomeScreen />; }
+return <AddTaskScreen />;
+
+}
